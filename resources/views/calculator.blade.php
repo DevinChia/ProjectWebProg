59df8746b37114b57881fec6cff193804ad6c5f9
Damar Pradnyana <134548390+F0rkiss@users.noreply.github.com>
2024-11-17 23:25:19 +0700

    Minor Update
    + Logo Icon
    + Logo Navbar
    + Font

--- resources/views/calculator.blade.php
+++ resources/views/calculator.blade.php
@@ -3,14 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Support</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
+    <title>Support</title>
+    
+    <link rel="icon" href="/image/justLogo.png">
+    <style>
+        *{
+            font-family: Roboto Slab;
+        }
+    </style>
 </head>
 <body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <nav class="navbar navbar-expand-lg" style="background-color:#113826">
         <div class="container-fluid">
             <a class="navbar-brand" href="{{ route('home') }}" style="color: white;">GIZIES</a>
+            <a class="navbar-brand" href="{{ route('home') }}" style="color: white;"> <img src="{{URL('image/logo.png')}}" alt="" style="max-width: 120px;"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
