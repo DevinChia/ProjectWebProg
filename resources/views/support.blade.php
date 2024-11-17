f1c914e969f169b9d3180370150b63ca401ea195
Terrence Reinhardt <terrencereinhardt041004@gmail.com>
2024-11-17 22:17:26 +0700

    Update support.blade.php

--- resources/views/support.blade.php
+++ resources/views/support.blade.php
@@ -8,38 +8,71 @@
 </head>
 <body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <nav class="navbar navbar-expand-lg" style="background-color:#4CAF50">
+    <nav class="navbar navbar-expand-lg" style="background-color:#113826">
         <div class="container-fluid">
-            <a class="navbar-brand" href="{{ route('home') }}" style="color: white">Gizies</a>
+            <a class="navbar-brand" href="{{ route('home') }}" style="color: white;">GIZIES</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
+                <ul class="navbar-nav ms-auto me-3" style="font-size: 1.2rem;">
                     <li class="nav-item">
-                        <a class="nav-link"  href="{{ route('home') }}" style="color: white">Home</a>
+                        <a class="nav-link active" aria-current="page" href="{{ route('home') }}" style="color: white;">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('program') }}" style="color: white">Programs</a>
+                        <a class="nav-link" href="{{ route('about') }}" style="color: white;">About Us</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('about') }}" style="color: white">About Us</a>
+                        <a class="nav-link" href="{{ route('program') }}" style="color: white;">Programs</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{ route('support') }}" style="color: yellow; font-weight:bold">Support</a>
+                        <a class="nav-link" href="{{ route('support') }}" style="color: white;">Support</a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: white">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: white;">
                             Tools
                         </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbar Dropdown MenuLink" style="background-color: #4CAF50">
-                            <li><a class="dropdown-item" href="{{ route('calculator') }}" style="color: white" onmouseover="this.style.backgroundColor='#EB8E27'" 
-                            onmouseout="this.style.backgroundColor='transparent'">Calculator</a></li>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink" style="background-color: #113826;">
+                            <li>
+                                <a class="dropdown-item" href="{{ route('calculator') }}" style="color: white;" onmouseover="this.style.backgroundColor='#EB8E27'" onmouseout="this.style.backgroundColor='transparent'">Calculator</a>
+                            </li>
                         </ul>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
+    <footer class="bg-light text-center text-md-start py-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 mb-3">
+                    <h5 class="text-uppercase">Gizies</h5>
+                    <p class="text-muted">Your trusted source for health and nutrition tips, meal plans, and diet programs.</p>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <h5 class="text-uppercase">Features</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="{{ route('home') }}" class="text-dark text-decoration-none">Home</a></li>
+                        <li><a href="{{ route('about') }}" class="text-dark text-decoration-none">About Us</a></li>
+                        <li><a href="{{ route('program') }}" class="text-dark text-decoration-none">Programs</a></li>
+                        <li><a href="{{ route('support') }}" class="text-dark text-decoration-none">Support</a></li>
+                        <li><a href="{{ route('calculator') }}" class="text-dark text-decoration-none">Tools</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <h5 class="text-uppercase">Follow Us</h5>
+                    <div>
+                        <a href="#" class="text-dark me-3"><i class="bi bi-facebook"></i></a>
+                        <a href="#" class="text-dark me-3"><i class="bi bi-twitter"></i></a>
+                        <a href="#" class="text-dark me-3"><i class="bi bi-instagram"></i></a>
+                        <a href="#" class="text-dark"><i class="bi bi-linkedin"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="text-center py-3">
+                <p class="mb-0">&copy; 2024 GIZIES. All rights reserved.</p>
+            </div>
+        </div>
+    </footer>    
 </body>
 </html>
