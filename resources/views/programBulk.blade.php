3bf97e6374400095d1b03f9161d0e2b78e1d703f
Damar Pradnyana <134548390+F0rkiss@users.noreply.github.com>
2024-12-22 15:03:31 +0700

    Add
    +ProgramBulk

--- resources/views/programBulk.blade.php
+++ resources/views/programBulk.blade.php
@@ -21,12 +21,32 @@
         }
 
         .hl{ /* horizontal Line */
+            border-top: 3px solid gray;
+            opacity: 30%;
+            height: 20px;
+
+            max-width: 80%;
+
+            position: relative;
+            left: 50%;
+            transform: translateX(-50%);
+
+            padding-bottom: 20px;
+        }
+
+        .hl-divider{ /* horizontal Line */
             border-top: 1px solid gray;
             opacity: 30%;
             height: 20px;
+
+            max-width: 60%;
+
+            position: relative;
+            left: 50%;
+            transform: translateX(-50%);
         }
 
-        .hl-header{ /* horizontal Line */
+        .hl-right{ /* horizontal Line */
             position: relative;
             left:35%;
             /* transform: translateX(-50%); */
@@ -38,8 +58,25 @@
 
             max-width: 100%;
 
-            overflow: hidden;
+            /* overflow: hidden; */
         }
+
+        .hl-left{ /* horizontal Line */
+            position: relative;
+            
+            /* transform: translateX(-50%); */
+            
+
+            border-top: 3px solid gray;
+            opacity: 30%;
+            height: 20px;
+
+            max-width: 65%;
+
+            /* overflow: hidden; */
+        }
+
+        
 
         .header-container {
             margin-top: 15px;
@@ -55,20 +92,20 @@
 
         .image-header {
             width: 80%;
-            height: 400px;
+            height: 32vw;
             border-radius: 10px;
         }
 
         .text-header{
             position: absolute;
-            top: 55%;
-            left: 73%;
+            top: 55vh;
+            left: 150vh;
             
             text-align: center;
         }
 
         .DietText{
-            font-size: 30px;
+            font-size: 2vw;
             font-weight: bold;
             color: white;
         }
@@ -77,20 +114,107 @@
             margin: 15px;
             margin-left:10%;
             margin-right:10%;
+            margin-bottom: 40px;
 
             display: flex;
             flex-direction: row;
             justify-content: center;
+
+            
         }
 
         .image-content{
-            width: 300px;
-            height: auto;
-            border-radius: 20px;
+            position: relative;
+            left: 50%;
+            transform: translateX(-50%);
+
+            width: 50vh;
+            /* height: auto; */
+            border-radius: 10px;
         }
 
         .gap{
             margin: 30px
+        }
+
+        .left-content{
+            
+
+            width: 50%;
+            text-align: justify;
+            width:100%;
+        }
+
+        .right-content{
+            width: 50%;
+            text-align: justify;
+
+            width:100%;
+        }
+
+        .text{
+            font-size: 2.8vh;
+        }
+
+        .mid-content{
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+        }
+
+        .head-content{
+            display: flex;
+            justify-content: center;
+        }
+
+        .card{
+            position: relative;
+            left: 0px;
+        }
+
+        .card-carousel{
+            display: flex;
+            justify-content: center;
+            gap: 20px;
+
+            margin-top: 20px;
+            margin-bottom: 20px;
+        }
+
+        .card:not(:first-child){
+            margin-left:-50px;
+        }
+
+        .card:hover{
+            transform: translateY(-20px);
+            transition-duration: 0.5s;
+        }
+
+        .card:hover ~ .card{
+            left: 20px;
+            transition-duration: 0.5s;
+
+        }
+
+        .text-card{
+            transition: .5s ease;
+            opacity: 0;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            -ms-transform: translate(-50%, -50%);
+            text-align: center;
+        }
+
+        .card:hover .text-card{
+            opacity: 1;
+        }
+
+        .card:hover .card-content{
+            transition-duration: 0.5s;
+            opacity: 0.5;
+            filter: blur(3px);
         }
         
     </style>
@@ -139,18 +263,85 @@
         </div>
     </div>
 
-    <div class="hl-header"></div>
+    <div class="hl-right"></div>
 
     <div class="content-container">
         <div class="left-content">
-            <img src="{{URL('image\image1.jpg')}}" alt="" class="image-content">
+            <!-- <img src="{{URL('image\image1.jpg')}}" alt="" class="image-content"> -->
+
+            <iframe class="image-content" width="560" height="315" src="https://www.youtube.com/embed/PiHAGXbLVno?si=biC9rYsFR5MahdKJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>s
         </div>
 
         <div class="gap"></div>
 
         <div class="right-content">
-            <h1>Bulking Program</h1>
-            <p> Bulking program is a great program for beginners. It is a 4-week program that will help you get started on your fitness journey. The program includes a mix of cardio and strength training exercises that will help you build muscle and burn fat. The program is designed to be challenging but achievable, so you can see results without feeling overwhelmed. </p>
+            <div class="text">
+                <h1>Bulking Program</h1>
+                <p> Bulking program is a great program for beginners. It is a 4-week program that will help you get started on your fitness journey. The program includes a mix of cardio and strength training exercises that will help you build muscle and burn fat. The program is designed to be challenging but achievable, so you can see results without feeling overwhelmed. </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="hl-left"></div>
+    
+    <div class="content-container">
+        <div class="left-content">
+            <div class="text">
+                <h1>Bulking Program</h1>
+                <p> Bulking program is a great program for beginners. It is a 4-week program that will help you get started on your fitness journey. The program includes a mix of cardio and strength training exercises that will help you build muscle and burn fat. The program is designed to be challenging but achievable, so you can see results without feeling overwhelmed. </p>
+            </div>
+        </div>
+        
+        <div class="gap"></div>
+        
+        <div class="right-content">
+            <img src="{{URL('image\image1.jpg')}}" alt="" class="image-content">
+        </div>
+    </div>
+
+    <div class="hl"></div>
+    
+    <div class="mid-content">
+        <div class="head-content">
+            <h1>Recomendation Food from our Personal Trainer</h1>
+        </div>
+
+        <div class="hl-divider"></div> <!-- Divider -->
+        
+        <div class="card-carousel">
+            <div class="card" style="width: 18rem;">
+                <img src="{{URL('image\image1.jpg')}}" alt="" class="card-content">
+                <div class="text-card">
+                    <h5 style="" class="">537 Calories</h5>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+            <img src="{{URL('image\image1.jpg')}}" alt="" class="card-content">
+                <div class="text-card">
+                    <h5 style="" class="">537 Calories</h5>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+            <img src="{{URL('image\image1.jpg')}}" alt="" class="card-content">
+                <div class="text-card">
+                    <h5 style="" class="">537 Calories</h5>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="{{URL('image\image1.jpg')}}" alt="" class="card-content">
+                    <div class="text-card">
+                        <h5 style="" class="">537 Calories</h5>
+                    </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="{{URL('image\image1.jpg')}}" alt="" class="card-content">
+                    <div class="text-card">
+                        <h5 style="" class="">537 Calories</h5>
+                    </div>
+            </div>
+            
+            <div class="hl-divider"></div> <!-- Divider -->
+        
         </div>
     </div>
 
